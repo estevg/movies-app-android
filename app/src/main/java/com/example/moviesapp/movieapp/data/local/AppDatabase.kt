febbd404e811aa8fb8791dd97afc558afc6e62e3
Esteban Vega <iestebanvega1@gmail.com>
2022-12-26 17:46:14 -0500

    AppDatabase

---
+++ app/src/main/java/com/example/moviesapp/movieapp/data/local/AppDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.moviesapp.movieapp.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.moviesapp.movieapp.data.model.MovieEntity
+
+@Database(entities = [MovieEntity::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            INSTANCE = INSTANCE ?: Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "movie_table"
+            ).build()
+            return INSTANCE!!
+        }
+
+        fun destroyDatabase(){
+            INSTANCE = null
+        }
+    }
+}
